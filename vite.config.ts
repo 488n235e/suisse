f82458ddaf5032d9f0045d7cf760c42b31438a7e
488n235e <488n235e@pm.me>
2023-07-12 02:25:03 +0200

    chore: initialize project

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+module.exports = defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'suisse',
+      fileName: format => `suisse.${format}.js`,
+    },
+  },
+  plugins: [dts()],
+});
